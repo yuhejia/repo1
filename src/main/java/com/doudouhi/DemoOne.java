03e8267c054ed0908e1ab261dd2a22a0f529244b
admin <peppadog@aliyun.com>
2021-06-05 01:21:25 +0800

    second commit

--- src/main/java/com/doudouhi/DemoOne.java
+++ src/main/java/com/doudouhi/DemoOne.java
@@ -6,5 +6,6 @@ package com.doudouhi;
 public class DemoOne {
     public static void main(String[] args) {
         System.out.println("hello git !!");
+        System.out.println("hello git 22222 !!");
     }
 }
