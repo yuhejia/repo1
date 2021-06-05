e5fbca77029885b998b51ab7a839edc0ed661594
admin <peppadog@aliyun.com>
2021-06-05 16:57:02 +0800

    hot-fix commit1

--- src/main/java/com/doudouhi/DemoOne.java
+++ src/main/java/com/doudouhi/DemoOne.java
@@ -16,6 +16,8 @@ public class DemoOne {
         list.add(3,"string");
         list.add(4,"string");
         list.add(5,"string");
+        list.add(6,"string");
+        list.add(7,"string");
         for (String s : list) {
             System.out.println(s);
         }
