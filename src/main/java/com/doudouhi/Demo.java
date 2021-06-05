d6e3523b3a59bebc50f63d754eaae20f1c77bc9b
yuhejia <68991774+yuhejia@users.noreply.github.com>
2021-06-05 22:03:45 +0800

    Update Demo.java
    github提交

--- src/main/java/com/doudouhi/Demo.java
+++ src/main/java/com/doudouhi/Demo.java
@@ -8,6 +8,7 @@ public class Demo {
         System.out.println("hello world!!");
         //vim 不可以打开，否则很难写code
         System.out.println("哈哈");
+        System.out.println("你很稀烂，但是我还是支持你");
 
     }
 }
