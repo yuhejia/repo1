ed61f93a37a7d91312b7851fd9692af02c130118
admin <peppadog@aliyun.com>
2021-06-05 21:36:19 +0800

    hot-fix commit5

--- src/main/java/com/doudouhi/Demo.java
+++ src/main/java/com/doudouhi/Demo.java
@@ -7,5 +7,7 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello world!!");
         //vim 不可以打开，否则很难写code
+        System.out.println("哈哈");
+
     }
 }
